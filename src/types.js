3e343d0fd8a9b77704f53467f880223f862a177e
natefrog808 <wetakatini@gmail.com>
2025-02-04 11:25:34 -1000

    Create types.js

---
+++ src/types.js
@@ -0,0 +1,15 @@
+export const CHALLENGE_TYPES = {
+  PATTERN: 'pattern',
+  OPTIMIZATION: 'optimization',
+};
+
+export const PATTERN_TYPES = {
+  ARITHMETIC: 'arithmetic',
+  GEOMETRIC: 'geometric',
+  QUADRATIC: 'quadratic',
+  EXPONENTIAL: 'exponential',
+};
+
+export const LEARNING_TRACKS = {
+  PATTERN_RECOGNITION: 'PATTERN_RECOGNITION',
+};
