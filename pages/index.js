492c11eee11580007a5f7016ecaa4215aca35c8e
natefrog808 <wetakatini@gmail.com>
2025-02-04 11:28:00 -1000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import TrainingEnvironment from '../src/TrainingEnvironment'; // Adjust path
+
+export default function Home() {
+  return (
+    <div className="p-4"> {/* Add some padding */}
+      <TrainingEnvironment />
+    </div>
+  );
+}
